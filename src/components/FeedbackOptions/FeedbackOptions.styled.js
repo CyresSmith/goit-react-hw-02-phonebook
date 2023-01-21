b1ae9b1b76203654ba6c34bc0aba2597f7d79437
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-01-21 21:58:12 +0200

    clean

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import theme from 'theme';
-
-export const List = styled.ul`
-  display: flex;
-  gap: ${theme.space[4]};
-`;
-
-export const Item = styled.li``;
